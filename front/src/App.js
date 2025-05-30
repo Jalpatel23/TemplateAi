303c9fe9b6b3397302a6a8ec80b85ec689953364
Jalpatel23 <jalpatel661@gmail.com>
2025-05-30 08:26:26 +0530

    removed some files

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,6 @@
 import {Routes,Route} from 'react-router-dom';
-import Login from './pages/Auth/login.js';
-import Signup from './pages/Auth/signup.js';
 import Home from './pages/home.js'
 import Error404 from './pages/pagenotfound.js';
-import Profile from './pages/profile.js';
-import PrivateRoute from './routes/private.js';
-import Temp from './pages/temp.js';
-import  Temp2 from './pages/temp2.js';
 
 
 
@@ -14,19 +8,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/temp2' element={<Temp2/>}/>
-        <Route path='/temp' element={<Temp/>}/>
-
-
         <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='*' element={<Error404/>}/>
-
-        <Route path="profile" element={<PrivateRoute/>}>
-          <Route path="" element={<Profile/>}/>
-        </Route>
-        
+        <Route path='*' element={<Error404/>}/>        
 
       </Routes>
     </>
